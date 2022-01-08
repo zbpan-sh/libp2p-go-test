99b888a174e230b8b70e1eab23935b00f12cbc31
PAN ZHI BIN <458085311@qq.com>
2022-01-08 16:32:20 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/libp2p/go-libp2p"
+)
+
+func main() {
+	node, err := libp2p.New()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Listen addresses:", node.Addrs())
+
+	if err := node.Close(); err != nil {
+		panic(err)
+	}
+}
+
